47153a3860b50ff26aad2f9df611e902d17f7e6c
princeigwe <igwep297@gmail.com>
2025-03-13 01:29:02 +0100

    get operations on user model

--- routers/users_router.py
+++ routers/users_router.py
@@ -9,3 +9,11 @@ router = APIRouter()
 @router.post("/users")
 async def create_user(user: UserDto, db: Session = Depends(get_db)):
   return await users_service.create_user(user, db)
+
+@router.get("/users")
+async def get_users(db: Session = Depends(get_db)):
+  return await users_service.get_users(db)
+
+@router.get("/users/{user_id}")
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+  return await users_service.get_user_by_id(user_id, db)
--- services/users_service.py
+++ services/users_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from database_config import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from dtos.user_dto import UserDto
 from models.user_model import User
 
@@ -11,3 +11,13 @@ async def create_user(body:UserDto, db: Session = Depends(get_db)):
   db.commit()
   db.refresh(new_user)
   return new_user
+
+
+async def get_users(db:  Session = Depends(get_db)):
+  return db.query(User).all()
+
+async def get_user_by_id(user_id: int, db: Session):
+  user =  db.query(User).filter(User.id == user_id).first()
+  if user is None:
+    raise HTTPException(status_code=404, detail="User not found")
+  return user
