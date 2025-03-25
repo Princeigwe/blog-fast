2bd1ec58660f86086efb815cd49f5a54480742b0
princeigwe <igwep297@gmail.com>
2025-03-25 14:09:17 +0100

    alembic setup

--- database_config.py
+++ database_config.py
@@ -1,13 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # this class maintains a collection of all the defined tables for sqlalchemy orm functionality
 # it is a base class for all the tables that are defined in the database
 Base = declarative_base()
 
+POSTGRES_USER=os.environ.get("POSTGRES_USER")
+POSTGRES_PASSWORD=os.environ.get("POSTGRES_PASSWORD")
+POSTGRES_DB=os.environ.get("POSTGRES_DB")
+POSTGRES_HOST=os.environ.get("POSTGRES_HOST")
+POSTGRES_PORT=os.environ.get("POSTGRES_PORT")
 
-DATABASE_URL = "sqlite:///./test.db" # test.db is the name of the database file
+# DATABASE_URL = "sqlite:///./test.db" # test.db is the name of the database file
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL) # create a database engine
 
