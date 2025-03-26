900a9bde60673f4eb455f835096001dbb2e6d996
princeigwe <igwep297@gmail.com>
2025-03-26 03:32:20 +0100

    RBAC

--- services/users_service.py
+++ services/users_service.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from database_config import get_db
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, status
 from dtos.user_dto import UserDto
 from models.user_model import User
 from email_validator import validate_email, EmailNotValidError
@@ -12,7 +12,7 @@ async def create_user(body:UserDto, db: Session = Depends(get_db)):
   existing_user_with_email = db.query(User).filter(User.email==body.email).first()
   existing_user_with_username = db.query(User).filter(User.username==body.username).first()
   if existing_user_with_email or existing_user_with_username:
-    raise HTTPException(status_code=400, detail="User with email or username already exists")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User with email or username already exists")
   
   new_user = User(email=body.email, username=body.username, password=body.password, role=body.role, location=body.location)
   db.add(new_user)
@@ -27,20 +27,20 @@ async def get_users(db:  Session = Depends(get_db)):
 async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
   user =  db.query(User).filter(User.id == user_id).first()
   if user is None:
-    raise HTTPException(status_code=404, detail="User not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
   return user
 
 
 async def update_user_by_id(user_id: int, body: UserDto, db: Session = Depends(get_db)):
   existing_user = db.query(User).filter(User.id == user_id).first()
   if existing_user is None:
-    raise HTTPException(status_code=404, detail="User does not exist")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist")
   
   # checking for user with the same email or username 
   existing_user_with_email = db.query(User).filter(User.email==body.email).first()
   existing_user_with_username = db.query(User).filter(User.username==body.username).first()
   if existing_user_with_email or existing_user_with_username:
-    raise HTTPException(status_code=400, detail="User with email or username already exists")
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User with email or username already exists")
   
   existing_user.email = body.email
   existing_user.username = body.username
@@ -52,7 +52,7 @@ async def update_user_by_id(user_id: int, body: UserDto, db: Session = Depends(g
 async def delete_user_by_id(user_id: int,  db: Session = Depends(get_db)):
   existing_user = db.query(User).filter(User.id == user_id).first()
   if existing_user is None:
-    raise HTTPException(status_code=404, detail="User does not exist")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist")
   
   db.delete(existing_user)
   db.commit()
